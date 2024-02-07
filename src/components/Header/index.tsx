b43a33a787bc565c11b3c4ff402651ab47153ab7
techinn0vation <techinnovation64@gmail.com>
2024-02-07 18:58:14 -0300

    Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,11 @@ export default function Header() {
   return (
     <WrapperHeader>
       <ContentHeader>
-        <LogoBranding src={Logo} alt='logo' priority={true} />
+        <LogoBranding
+          src={Logo}
+          alt='grupo gestão imobiliária'
+          priority={true}
+        />
       </ContentHeader>
     </WrapperHeader>
   )
